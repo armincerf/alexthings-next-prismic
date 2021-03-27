05d390488b1ab364c778413a1c5683b029a96322
Alex Davis <alx@juxt.pro>
2021-03-27 14:54:54 +0000

    change logo

--- components/Header.js
+++ components/Header.js
@@ -2,24 +2,30 @@ import React from 'react';
 import NextLink from 'next/link';
 import { Navigation, LanguageSwitcher } from '.';
 
-const Header = ({ menu, altLangs }) => (
-  <>
-    <header>
-      <div className="menu">
-        <NextLink href={'/'} passHref>
-          <a>
-            <img className="logo" src="/images/logo.png" alt="Site logo" />
-          </a>
-        </NextLink>
-      </div>
-      <div className="menu">
-        <ul>
-          <Navigation menu={menu} />
-          <LanguageSwitcher altLangs={altLangs} />
-        </ul>
-      </div>
-    </header>
-  </>
+const Header = ({ menu, altLangs }) => ( <
+    >
+    <
+    header >
+    <
+    div className = "menu" >
+    <
+    NextLink href = { '/' }
+    passHref >
+    logo <
+    /NextLink> < /
+    div > <
+    div className = "menu" >
+    <
+    ul >
+    <
+    Navigation menu = { menu }
+    /> <
+    LanguageSwitcher altLangs = { altLangs }
+    /> < /
+    ul > <
+    /div> < /
+    header > <
+    />
 );
 
 export default Header;
